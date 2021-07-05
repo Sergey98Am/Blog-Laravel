22008f8f146b9f1eb5c62573021c5c95c42ce32b
Sergey98Am <serg98barca@gmail.com>
2021-07-05 10:14:29 +0400

    added permissions CRUD

---
+++ app/Http/Requests/UpdatePermissionRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class UpdatePermissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+//        return Gate::allows('permission_edit');
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+        ];
+    }
+}
